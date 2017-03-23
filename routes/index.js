0845efc189d201edf07414df8130b22c731b225f
daviliyo <daviliyo.tk@gmail.com>
2017-03-23 19:17:29 +0100

    Pujada amb les rutes creades. v1.0.0

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Index' });
+});
+
+router.get('/tareas', function(req, res, next) {
+  res.render('tasks', { title: 'Tareas' });
+});
+
+router.get('/usuarios', function(req, res, next) {
+    res.render('users', { title: 'Usuarios' });
 });
 
 module.exports = router;
